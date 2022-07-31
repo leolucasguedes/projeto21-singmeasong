9315ddbf292238cff5fcb466790787bea23ad23d
Leonardo Lucas <leolucasguedes@yahoo.com.br>
2022-07-31 14:28:08 -0300

    initial configs

--- back-end/jest.config.js
+++ back-end/jest.config.js
@@ -1,5 +1,6 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-export default {
+module.exports = {
+  setupFilesAfterEnv: ["./jest.setup.js"],
   preset: "ts-jest",
   testEnvironment: "node",
   extensionsToTreatAsEsm: [".ts"],
